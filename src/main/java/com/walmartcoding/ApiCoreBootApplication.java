23e3aa130e1fe2e38c522d876ea811e8a7965b56
morgan <zhengsz@vt.edu>
2018-09-28 15:08:32 -0400

    Created Spring Boot Appllication

---
+++ src/main/java/com/walmartcoding/ApiCoreBootApplication.java
@@ -0,0 +1,13 @@
+package com.walmartcoding;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCoreBootApplication {
+
+    public static void main (String[] args){
+        SpringApplication.run(ApiCoreBootApplication.class, args);
+    }
+}
