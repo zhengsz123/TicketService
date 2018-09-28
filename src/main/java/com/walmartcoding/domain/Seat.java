7ba55ac6cb53eb9d04d7420c2d665f502edbd041
morgan <zhengsz@vt.edu>
2018-09-28 16:21:15 -0400

    Created User and Seat model in Domain layer

--- src/main/java/com/walmartcoding/domain/Seat.java
+++ src/main/java/com/walmartcoding/domain/Seat.java
@@ -1,4 +1,62 @@
 package com.walmartcoding.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name = "users")
 public class Seat {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "seats_id_seq")
+    @SequenceGenerator(name = "seats_id_seq", sequenceName = "seats_id_seq")
+    private long id;
+    @Column(name = "col")
+    private int col;
+    @Column(name = "row")
+    private int row;
+    @Column(name = "status")
+    private String status;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "users_id")
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/walmartcoding/domain/User.java
+++ src/main/java/com/walmartcoding/domain/User.java
@@ -1,4 +1,51 @@
 package com.walmartcoding.domain;
 
+
+
+import javax.persistence.*;
+import static javax.persistence.GenerationType.SEQUENCE;
+
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq")
+    private long id;
+
+    @Column(name="first_name")
+    private String firstname;
+    @Column(name="last_name")
+    private String lastname;
+    @Column(name="email")
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
